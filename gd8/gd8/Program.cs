737287cf839061a030fed2e146af51007f933463
Arear <119037037+Arear2004@users.noreply.github.com>
2023-04-21 17:54:09 +0300

    Add files via upload

---
+++ gd8/gd8/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gd8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] mass = new int[10];
+            int j = mass.Length - 1;
+            Random random = new Random();
+            for (int i = 0; i < 10; i++)
+            {
+                mass[i] = random.Next(0, 10);
+            }
+            Console.WriteLine(string.Join(", ", mass));
+            mass = mass.OrderBy(x => x == 0 ? 1).ToArray();
+            Console.WriteLine(string.Join(", ", mass));
+        }
+    }
+}
